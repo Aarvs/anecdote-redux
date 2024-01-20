8acb30f7491b0b33f44b71d441412040d7d06806
Arnav <arnavsh2004@gmail.com>
2024-01-20 21:11:18 +0530

    :sparkles: Filter reducer
    Add a filter reducer to filter anecdotes with a given input

--- src/components/AnecdoteList.jsx
+++ src/components/AnecdoteList.jsx
@@ -3,13 +3,9 @@ import { addVote } from "../reducers/anecdoteReducer";
 
 const AnecdoteList = () => {
     const dispatch = useDispatch();
-    const anecdotes = useSelector(state => state)
-
-    // const vote = (id) => {
-    //   console.log('vote', id)
-    //   dispatch(addVote(id))
-    // }
-
+    const anecdotes = useSelector(state => state.anecdotes)
+    const filter = useSelector(state => state.filter)
+    
     const Anecdote = ({anecdote, handleClick}) => {
         return(
             <li>
@@ -24,7 +20,8 @@ const AnecdoteList = () => {
         )
     }
 
-    const sortedAnecdotes = anecdotes.sort((a,b) => b.votes - a.votes)
+    const filteredAnecdotes = anecdotes.filter(anecdote => anecdote.content.toLowerCase().includes(filter.toLowerCase()))
+    const sortedAnecdotes = filteredAnecdotes.sort((a,b) => b.votes - a.votes)
 
     return(
         <ul>
