9038d8c0c1f561cdeec228ecac2abc1131b036a4
Arnav <arnavsh2004@gmail.com>
2024-01-27 13:52:52 +0530

    :art: Redux Thunk library
    modify the initialization and creation of anecdotes using asynchronous action creaters, made possible by the redux thunk library

--- src/components/AnecdoteForm.jsx
+++ src/components/AnecdoteForm.jsx
@@ -1,7 +1,6 @@
 import { useDispatch } from "react-redux";
-import { createAnecdote } from "../reducers/anecdoteReducer";
+import { appendAnecdote } from "../reducers/anecdoteReducer";
 import { setNotification, removeNotification } from "../reducers/notificationReducer";
-import anecdoteService from "../services/anecdotes"
 
 const NewAnecdote = () => {
     const dispatch = useDispatch()
@@ -9,8 +8,7 @@ const NewAnecdote = () => {
     const addAnecdote = async (event) => {
         event.preventDefault()
         const content = event.target.anecdote.value
-        const newAnecdote = await anecdoteService.createNew(content)
-        dispatch(createAnecdote(newAnecdote))
+        dispatch(appendAnecdote(content))
         dispatch(setNotification(`You created ${content}`))
         setTimeout(() => {
           dispatch(removeNotification())
